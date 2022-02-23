b3ea9f62184db8af989beb5ad313839969c2f419
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-02-23 17:36:57 -0300

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let idade = document.getElementById('num1')
+let result = document.getElementById('res')
+
+function calcular() {
+    let ida = Number(idade.value)
+    if(ida < 16) {
+        result.innerHTML = `Você tem ${ida} ou seja... Não vota`
+    } else if (ida >= 18) {
+        result.innerHTML = `Você tem ${ida} ou seja...Obrigatório`
+    } if(ida >= 16 || ida >= 65) {
+        result.innerHTML = `Você tem ${ida} ou seja...Opcional`
+    }
+}
